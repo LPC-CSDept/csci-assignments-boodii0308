190e784ca0869d80d44da55e16265928fe1f29f6
boodii0308 <boodii.tb@gmail.com>
2021-12-16 18:56:19 -0800

    lol

--- Final/q5.asm
+++ Final/q5.asm
@@ -1,24 +1,39 @@
 #q5.asm
+
 #Question 1
+
 #Tengisbold Batsaikhan
+
 #12/16/2021
+
+    .data
+msg: .asciiz "Enter number: "
 
     .text
     .globl main
 main:
-    lw $t9, 3
+    la  $a0, msg
+    syscall
+
+    ori $s7, 3
     lui $t1, 0xffff    #load upper imm to get the address of receiver
 wait:
-    lw $t2, 0($t1)      #to check receiver control
+    lw      $t2, 0($t1)      #to check receiver control
     andi    $t2, $t2, 0x0001        #to check receiver control ready bit is 1 or 0
-    beq    $t2, $zero, wait
-    lw      $s0, 4($t0)
+    beq     $t2, $zero, wait
+
+    lw      $s0, 4($t2)
     sub      $s0, $s0, 48
     mul     $s0, $s0, 10
-    add     $s1, $s1, $s1
-    sub     $t9, $t9, 1
-    beq    $t9, $zero, wait
+    nop
 
+    add     $s1, $s1, $s1
+    sub     $s7, $s7, 1
+    beq    $t9, $zero, print
+    nop
+    
+
+print:
     add $a0, $s1, $zero
     li $v0, 1
     syscall
