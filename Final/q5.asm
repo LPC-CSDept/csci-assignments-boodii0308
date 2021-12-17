a90a829e289cb27f743e656c09c5ce13f20db06c
boodii0308 <boodii.tb@gmail.com>
2021-12-16 19:40:22 -0800

    aa

--- Final/q5.asm
+++ Final/q5.asm
@@ -15,24 +15,28 @@ main:
     la  $a0, msg
     syscall
 
+    ori $s1, $s1, $zero
     ori $s7, 3
     ori $s6, 10
     lui $t0, 0xffff    #load upper imm to get the address of receiver
+
 wait:
     lw      $t1, 0($t0)      #to check receiver control
     andi    $t1, $t1, 0x0001        #to check receiver control ready bit is 1 or 0
     beq     $t1, $zero, wait
+    nop
+
     lw      $s0, 4($t0)
-  
-    sub     $s0, $s0, 48
-    mul     $s0, $s0, $s6 
-    add     $s1, $s1, $s0
-    sub     $s7, $s7, 1
-    beq     $s7, $zero, print
 
-    
+    addi     $s0, $s0, -48    
+    mult     $s0, $s6
+    mflo        $s1
+    addu     $s1, $s1, $s0
+    addi     $s7, $s7, -1
+    beqz     $s7, wait
 
-print:
+    nop
+
     add $a0, $s1, $zero
     li $v0, 1
     syscall
